ceacaae618fc9fd229eec3cd54b611688b21d7d1
fridokus <oskarfr@student.chalmers.se>
2017-06-02 11:48:11 +0200

    musicalchairs.py

---
+++ musicalchairs.py
@@ -0,0 +1,36 @@
+import falcon
+import json
+
+size = 4
+
+grid = [[0 for i in range(size)] for j in range(size)]
+
+class GameBoard(object):
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = str(size)
+
+class ScanBoard(object):
+    def on_get(self, req, resp, x, y):
+        resp.status = falcon.HTTP_200
+        x = int(x)
+        y = int(y)
+        body = {'chairId': grid[x][y]}
+        resp.body = json.dumps(body)
+
+class SitDown(object):
+    def on_get(self, req, resp, x, y):
+        resp.status = falcon.HTTP_200
+
+# def addRoute(obj, route):
+#     api = falcon.API()
+#     api.add_route(route, obj)
+
+# addRoute(GameBoard(), '/board')
+# addRoute(ScanBoard(), '/scan/{x}/{y}')
+
+app = falcon.API()
+gameBoard = GameBoard()
+scanBoard = ScanBoard()
+app.add_route('/board', gameBoard)
+app.add_route('/scan/{x}:{y}', scanBoard)
