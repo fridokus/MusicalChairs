c9367c55b440458602bd93d1d83c0cfcec72c50d
fridokus <oskarfr@student.chalmers.se>
2017-06-08 13:53:48 +0200

    musicalchairs.py

--- musicalchairs.py
+++ musicalchairs.py
@@ -1,5 +1,6 @@
 import falcon 
 import json
+import psycopg2
 
 size = 4
 
@@ -11,11 +12,28 @@ class GameBoard(object):
         resp.body = str(size)
 
 class ScanBoard(object):
-    def on_get(self, req, resp, x, y):
+    def on_get(self, req, resp, playerId, x, y):
         resp.status = falcon.HTTP_200
-        x = int(x)
-        y = int(y)
-        body = {'chairId': grid[x][y]}
+        SQL = "SELECT scans_left FROM player WHERE id=(%s)"
+        data = str(playerId)
+        cur.execute(SQL, data)
+        row = cur.fetchone()
+        scansLeft = row[0]
+        if scansLeft <= 0:
+            body = {'status': 'No scans left', 'chairId': -1}
+        else:
+            SQL = "SELECT id FROM chair WHERE coordinates='{%s,%s}'"
+            x = 2
+            y = 3
+            data = (x, y)
+            cur.execute(SQL, data)
+            row = cur.fetchone()
+            if row:
+                chairId = row[0]
+                body = {'status': 'Chair found', 'chairId': chairId}
+            else:
+                body = {'status': 'No chair found', 'chairId': 0}
+
         resp.body = json.dumps(body)
 
 class SitDown(object):
@@ -29,6 +47,34 @@ class SitDown(object):
 
 # addRoute(GameBoard(), '/board')
 # addRoute(ScanBoard(), '/scan/{x}/{y}')
+
+conn = psycopg2.connect(dbname = 'musicalchairs', user = 'oskar', password = 'bajs', host = 'localhost')
+cur = conn.cursor()
+cur.execute("""select * from player""")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+playerId = 1
+SQL = """SELECT scans_left FROM player WHERE id=(%s)"""
+data = str(playerId)
+cur.execute(SQL, data)
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+SQL = "SELECT id FROM chair WHERE coordinates='{%s,%s}'"
+x = 2
+y = 3
+data = (x, y)
+cur.execute(SQL, data)
+row = cur.fetchone()
+if row:
+    chairId = row[0]
+    body = {'status': 'Chair found', 'chairId': chairId}
+else:
+    body = {'status': 'No chair found', 'chairId': 0}
+print(body)
 
 app = falcon.API()
 gameBoard = GameBoard()
